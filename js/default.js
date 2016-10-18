07c63f6989b73e8ba32878e9f894877df936fda4
Oscar Johansson <kodninja@Oscars-MacBook-Pro.local>
2016-10-18 23:30:26 +0200

    blog posts

--- js/default.js
+++ js/default.js
@@ -11,6 +11,9 @@ catch(e) {
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-74908203-1', 'auto');
-ga('send', 'pageview');
-dataLayer = [];
+if (window.location.host === 'tvattrad.nu') {
+    ga('create', 'UA-74908203-1', 'auto');
+    ga('send', 'pageview');
+    dataLayer = [];
+}
+
